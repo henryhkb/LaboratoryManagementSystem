42a0f7e6dccac1d7cad78428f32c653731d0d987
henry <henry@Floras-MacBook-Air.local>
2023-10-07 16:32:50 +0000

    Laboratory management system

---
+++ resources/views/login.blade.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+	
+<!-- Mirrored from laravel.spruko.com/azira/leftmenu_light/signup by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 16 Nov 2022 08:38:10 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+		<meta charset="UTF-8">
+		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="Description" content="Bootstrap Responsive Admin Web Dashboard HTML5 Template">
+		<meta name="Author" content="Spruko Technologies Private Limited">
+		<meta name="Keywords" content="admin,admin dashboard,admin dashboard template,admin panel template,admin template,admin theme,bootstrap 4 admin template,bootstrap 4 dashboard,bootstrap admin,bootstrap admin dashboard,bootstrap admin panel,bootstrap admin template,bootstrap admin theme,bootstrap dashboard,bootstrap form template,bootstrap panel,bootstrap ui kit,dashboard bootstrap 4,dashboard design,dashboard html,dashboard template,dashboard ui kit,envato templates,flat ui,html,html and css templates,html dashboard template,html5,jquery html,premium,premium quality,sidebar bootstrap 4,template admin bootstrap 4"/>
+		<!-- Title -->
+		<title> Azira -  Premium dashboard ui bootstrap rwd admin html5 template </title>
+
+		<!--- Favicon -->
+		<link rel="icon" href="assets/img/brand/favicon.png" type="image/x-icon"/>
+
+		<!--- Icons css -->
+		<link href="assets/css/icons.css" rel="stylesheet">
+
+		
+		<!--- Right-sidemenu css -->
+		<link href="assets/plugins/sidebar/sidebar.css" rel="stylesheet">
+
+		<!--- Custom Scroll bar -->
+		<link href="assets/plugins/mscrollbar/jquery.mCustomScrollbar.css" rel="stylesheet"/>
+
+		<!--- Style css -->
+		<link href="assets/css/style.css" rel="stylesheet">
+		<link href="assets/css/skin-modes.css" rel="stylesheet">
+
+		<!--- Sidemenu css -->
+		<link href="assets/css/sidemenu.css" rel="stylesheet">
+
+		<!--- Animations css -->
+		<link href="assets/css/animate.css" rel="stylesheet">
+		
+		<!--- Switcher css -->
+		<link href="assets/switcher/css/switcher.css" rel="stylesheet">
+		<link href="assets/switcher/demo.css" rel="stylesheet">	</head>
+	
+	<body class="main-body">
+	<!-- Loader -->
+		<div id="global-loader">
+			<img src="assets/img/loaders/loader-4.svg" class="loader-img" alt="Loader">
+		</div>
+		<!-- /Loader -->
+				<!-- main-signin-wrapper -->
+		<div class="my-auto page page-h">
+			<div class="main-signin-wrapper">
+				<div class="main-card-signin d-md-flex wd-100p">
+				<div class="wd-md-50p login d-none d-md-block page-signin-style p-5 text-white" >
+					<div class="my-auto authentication-pages">
+						<div class="text text-center">
+							
+							<h5 class="mb-4">Laboratory Management System</h4>
+							
+						</div>
+					</div>
+				</div>
+				<div class="p-5 wd-md-50p">
+					<div class="main-signin-header">
+						<h2>Welcome</h2>
+						<h4>Please sign in</h4>
+						<form>
+							<div class="form-group">
+								<label>Username</label><input class="form-control" placeholder="Enter your username" type="text">
+							</div>
+							<div class="form-group">
+								<label>Password</label> 
+								<input class="form-control" placeholder="Enter your password" type="password" value="sprukodemo">
+							</div>
+
+							<div>
+								<a href="{{url('/dashboard')}}"   class="btn btn-main-primary btn-block">Sign In</a>
+							</div>
+						</form>
+					</div>
+				
+				</div>
+			</div>
+			</div>
+		</div>
+		<!-- /main-signin-wrapper -->
+		
+		<!-- Back-to-top -->
+		<a href="#top" id="back-to-top"><i class="las la-angle-double-up"></i></a>
+
+		<!-- Back-to-top -->
+		<a href="#top" id="back-to-top"><i class="las la-angle-double-up"></i></a>
+
+		<!--- JQuery min js -->
+		<script src="assets/plugins/jquery/jquery.min.js"></script>
+
+		<!--- Datepicker js -->
+		<script src="assets/plugins/jquery-ui/ui/widgets/datepicker.js"></script>
+
+		<!--- Bootstrap Bundle js -->
+		<script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+		<!--- Ionicons js -->
+		<script src="assets/plugins/ionicons/ionicons.js"></script>
+
+		
+		<!--- Moment js -->
+		<script src="assets/plugins/moment/moment.js"></script>
+
+		<!--- JQuery sparkline js -->
+		<script src="assets/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
+
+		<!--- Perfect-scrollbar js -->
+		<script src="assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+		<script src="assets/plugins/perfect-scrollbar/p-scroll.js"></script>
+
+
+		<!--- Rating js -->
+		<script src="assets/plugins/rating/jquery.rating-stars.js"></script>
+		<script src="assets/plugins/rating/jquery.barrating.js"></script>
+
+		<!--- Custom Scroll bar Js -->
+		<script src="assets/plugins/mscrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+
+
+		<!--- Sidebar js -->
+		<script src="assets/plugins/side-menu/sidemenu.js"></script>
+
+
+		<!--- Right-sidebar js -->
+		<script src="assets/plugins/sidebar/sidebar.js"></script>
+		<script src="assets/plugins/sidebar/sidebar-custom.js"></script>
+		
+		<!--- Eva-icons js -->
+		<script src="assets/js/eva-icons.min.js"></script>
+
+		<!--- Scripts js -->
+		<script src="assets/js/script.js"></script>
+
+		<!--- Custom js -->
+		<script src="assets/js/custom.js"></script>
+		
+		<!--- Switcher js -->
+		<script src="assets/switcher/js/switcher.js"></script>
+	
+	</body>
+
+<!-- Mirrored from laravel.spruko.com/azira/leftmenu_light/signup by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 16 Nov 2022 08:38:10 GMT -->
+</html>
+
